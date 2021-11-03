1fa1141d9ac12886b46f5dab045720baf1882a50
Malin Ekenvärn <50721579+malinekenvarn@users.noreply.github.com>
2021-11-03 09:14:56 +0100

    Very very simple design as a blueprint to a gym website

---
+++ script.js
@@ -0,0 +1,8 @@
+/*A script that makes a function that shows the navigation links when clicking the hamburger menu link on a mobile version*/
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('nav-links')[0];
+
+toggleButton.addEventListener('click', ()=>{
+    navbarLinks.classList.toggle('active')
+})
